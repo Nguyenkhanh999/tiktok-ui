ef5331d4f98c0c153172ca067cf726b3fb034298
Nguyenkhanh999 <khanhnguyenit02@gmail.com>
2024-04-09 01:08:50 +0700

    update 1:08 9 4 2024

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
+import { Fragment } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { publicRoutes } from '~/routes'
+import { publicRoutes } from '~/routes';
+import { DefaultLayout } from './components/Layout';
 function App() {
     return (
         <Router>
-            <div className='App'>
+            <div className="App">
                 <Routes>
                     {publicRoutes.map((route, index) => {
-                        const Page = route.component
-                        return <Route key={route} path={route.path} element={<Page/>}/>
+                        const Page = route.component;
+
+                        let Layout = DefaultLayout;
+
+                        if (route.layout) {
+                            Layout = route.layout;
+                        } else if (route.layout === null) {
+                            Layout = Fragment;
+                        }
+
+                        return (
+                            <Route
+                                key={route}
+                                path={route.path}
+                                element={
+                                    <Layout>
+                                        <Page />
+                                    </Layout>
+                                }
+                            />
+                        );
                     })}
                 </Routes>
             </div>
---
+++ src/components/Layout/HeaderOnly/Header/index.jsx
@@ -0,0 +1,5 @@
+function Header() {
+    return <h2> Phần đầu</h2>;
+}
+
+export default Header;
