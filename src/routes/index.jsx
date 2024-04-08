b8d1073d322fcb159c6ec283c2ccb5c2b1d7b8d8
khanhnv3 <khanhnv3@rikkeisoft.com>
2024-04-08 18:49:33 +0700

    update 8 4 2024

---
+++ src/routes/index.jsx
@@ -0,0 +1,10 @@
+// Public routes
+import Home from '~/pages/Home'
+import Following from '~/pages/Following'
+const publicRoutes = [
+    { path: "/", component: Home },
+    { path: "/following", component: Following },
+];
+
+const privateRoutes = [];
+export { publicRoutes, privateRoutes}
